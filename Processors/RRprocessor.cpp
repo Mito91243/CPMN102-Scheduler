97f36e398e6ec0192f5124a74f33cff5a133dc1a
AhmedAshry7 <121507692+AhmedAshry7@users.noreply.github.com>
2023-04-11 22:08:25 +0200

    Add files via upload

---
+++ Processors/RRprocessor.cpp
@@ -0,0 +1,100 @@
+#include "RRprocessor.h"
+RRprocessor::RRprocessor(int ts) {
+	timeslice = ts;
+	running = nullptr;
+}
+void RRprocessor:: addtoready(process* pr,int T) {
+	if (running == nullptr) {
+		running = pr;
+		running->setstate('RUN');
+		if (running->getRT() == 0) {
+			running->setRT(T);
+		}
+		return;
+	}
+	else {
+		Q1.enqueue(pr);
+		running->setstate('RDY');
+	}
+}
+process* RRprocessor::Schedulealgo() {
+	process* pr;
+	bool z = Q1.dequeue(pr);
+	if (z)
+		return pr;
+	else
+		return nullptr;
+}
+/*process* RRprocessor::isfinished(int T) {
+	running->incrementWON();
+	if (running->getWON() == running->getCT()) {
+		process* finished = running;
+		process* pr;
+		bool z = Q1.dequeue(pr);
+		if (!z) {
+			return finished;
+		}
+		else {
+			running = pr;
+			running->setstate('r');
+			if (running->getRT() == 0) {
+				running->setRT(T);
+			}
+			return finished;
+		}
+
+	}
+	else return nullptr;
+}*/
+
+/*process* RRprocessor::needsIO(int T) {
+	if (running->getIO() == running->getNIO()) {
+		return nullptr;
+	}
+	if (running->getWON() == running->getIO_R()) {
+		process* finished = running;
+		process* pr;
+		bool z = Q1.dequeue(pr);
+		if (!z) {
+			return finished;
+		}
+		else {
+			running = pr;
+			running->setstate('r');
+			if (running->getRT() == 0) {
+				running->setRT(T);
+			}
+			return finished;
+		}
+
+	}
+	else return nullptr;
+}*/
+process* RRprocessor::reachedts(int T) {
+	TOP++;
+	if (TOP == timeslice) {
+		process* finished = running;
+		process* pr;
+		bool z = Q1.dequeue(pr);
+		if (!z) {
+			return finished;
+		}
+		else {
+			running = pr;
+			running->setstate('r');
+			if (running->getRT() == 0) {
+				running->setRT(T);
+			}
+			return finished;
+		}
+
+	}
+	else return nullptr;
+
+}
+process* RRprocessor::getrun() {
+	return running;
+}
+Queue<process*>RRprocessor::getqueue() {
+	return Q1;
+}
---
+++ Processors/RRprocessor.h
@@ -0,0 +1,21 @@
+#pragma once
+#include"Processor.h"
+#include"process.h"
+#include"Queue.h"
+
+class RRprocessor: public Processor
+{
+private:
+	int timeslice;
+	Queue<process*> Q1;
+	process* running;
+	int TOP;
+public:
+	RRprocessor(int ts);
+	virtual void addtoready(process* pr,int T);
+	virtual process* Schedulealgo();
+	process* reachedts(int T);
+	process* getrun();
+	Queue<process*> getqueue();
+};
+
