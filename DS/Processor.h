a38fe31756d91e5ccafc04f907a68567c4cb3fda
AhmedAshry7 <121507692+AhmedAshry7@users.noreply.github.com>
2023-04-11 21:45:07 +0200

    Update Processor.h

--- DS/Processor.h
+++ DS/Processor.h
@@ -5,7 +5,19 @@ class Processor
 protected:
 	process* running; // pointer to what the process is working on
 public:
-	virtual process* isfinished()
+	virtual process* changerun(int T) {
+		if (running != nullptr)
+		{
+			process* temp = running;
+			running = Schedulealgo();
+			if (running->getRT() == 0) {
+				running->setRT(T);
+			}
+			return temp;
+		}
+		return nullptr;
+	}
+	virtual process* isfinished(int T)
 	{
 		if (running != nullptr)
 		{
@@ -15,6 +27,9 @@ public:
 			{
 				running = Schedulealgo();
 				running->setstate('RUN');
+				if (running->getRT() == 0) {
+					running->setRT(T);
+				}
 				temp->setstate('TRM');
 				return temp;
 
@@ -27,15 +42,19 @@ public:
 	//if no returns a nullptr
 	virtual void addtoready(process* pr) = 0;//adds to the list
 	virtual process* Schedulealgo() = 0;
-	virtual process* needsIO()
+	virtual process* needsIO(int T)
 	{
 		process* temp = running;
 		if (running->getWON() == running->getIO_R())
 		{
 			running = Schedulealgo();
+			if (running->getRT() == 0) {
+				running->setRT(T);
+			}
 			temp->setstate('BLK');
 			return temp;
 		}
+		else return nullptr;
 	}
 	//checks if WON is equal to its next IO_R,and if yes changes the running pointer to the its next process sets its state to R and sets its RT
 	//and returns a pointer to the one that needs IO to be added to the blocked
