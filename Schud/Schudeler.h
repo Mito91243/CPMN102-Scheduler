390a9a85788c818df8ba0f5053f8e171801483c7
Mito91243 <85618190+Mito91243@users.noreply.github.com>
2023-04-16 03:43:56 +0200

    Update Schudeler.h

--- Schud/Schudeler.h
+++ Schud/Schudeler.h
@@ -1,4 +1,11 @@
+#pragma once
+#include "process.h"
+#include "Processor.h"
+#include "SJF.h"
+#include "Queue.h"
 #include "Node.h"
+#include "UI.h"
+#include "LinkedList.h"
 #include <iostream>
 
 using namespace std;
@@ -7,29 +14,32 @@ class Schudeler
 {
 private:
 	
+	UI*pUI;
 	Queue<process*> NewList;
-	//Queue<process*> ReadyList; -- > Each processor got 1
-	SJF ps;
 	Queue<process*> BlockedList;
 	Queue<process*> TerminatedList;
-	
-	int timestep = 0;
-	int nFCFS;
-	int nSJF;
-	int nRR;
+	Processor** pArr;
+	int processornum;
+	int processnum;
+	int timestep;
+	int Acounter;
+	LinkedList<int> PIDs;
+	LinkedList<int> SIGs;
 
 public:
 
+\
 	Schudeler();
 	template<typename A, typename B>
-	void MoveProcess(A Old, B New);
-	
-	template<typename A, typename B>
 	void MoveProcessor(A Old, B New);
-
+	void Terminate(process* p);
 	void PrintInfo();
-	void Execute(int timestep);  //Chooses which process goes to which processor
 	void LoadInput();
 	void Run();
 	void Allocate();
+	void Simulate();
+	~Schudeler();
 };
+
+
+
