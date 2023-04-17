77b39ac7d735ec2c2ac384cf9e8090294e6b75cf
Mito91243 <85618190+Mito91243@users.noreply.github.com>
2023-04-17 22:53:40 +0200

    Update UI.h

--- UI.h
+++ UI.h
@@ -10,17 +10,17 @@
 #include <iostream>
 using namespace std;
 
-class UI 
+class UI
 {
 public:
 	UI()
 	{
 	}
-	void updateUI(int &t,int &npr,Processor* P[],Queue<process*> &B,Queue<process*> &T)
+	void updateUI(int& t, int& npr, Processor* P[], Queue<process*>& B, Queue<process*>& T)
 	{
 		system("cls");
-		cout << "Current Timestep:" << t << endl;
-		cout << '\t' << "RDY processes" << '\t' << endl;
+		cout << "Current Timestep:" << t << endl << endl;
+		cout << '\t' << "--------------------------RDY processes--------------------------" << '\t' << endl;
 		for (int i = 0; i < npr; i++)
 		{
 			cout << "processor " << i;
@@ -33,11 +33,11 @@ public:
 			P[i]->printdata();
 			cout << endl;
 		}
-		cout << '\t' << "BLK processes" << '\t' << endl;
+		cout << '\t' << "--------------------------BLK processes--------------------------" << '\t' << endl;
 		cout << B.getCount() << " BLK: ";
 		B.printq();
-		cout << endl;
-		cout << '\t' << "RUN processes" << '\t' << endl;
+		cout << endl << endl;
+		cout << '\t' << "--------------------------RUN processes--------------------------" << '\t' << endl;
 		int c = 0;
 		for (int i = 0; i < npr; i++)
 		{
@@ -51,11 +51,11 @@ public:
 		{
 			if (P[i]->getrun())
 			{
-				cout << *(P[i]->getrun())<<"(P"<<i<<") ";
+				cout << *(P[i]->getrun()) << "(P" << i << ") ";
 			}
 		}
-		cout << endl;
-		cout << '\t' << "TRM processes" << '\t' << endl;
+		cout << endl << endl;
+		cout << '\t' << "--------------------------TRM processes--------------------------" << '\t' << endl;
 		cout << T.getCount() << " TRM: ";
 		T.printq();
 		cout << endl;
