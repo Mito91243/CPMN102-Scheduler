fd7c45fd7a37e9db4cce51882cd30321c5857a6f
AhmedAshry7 <121507692+AhmedAshry7@users.noreply.github.com>
2023-05-13 08:53:06 +0300

    Update SJF.h

--- Processors/SJF.h
+++ Processors/SJF.h
@@ -1,40 +1,15 @@
 #pragma once
-#include"PiorityQueue.h"
+#include"PriorityQueue.h"
 #include"Processor.h"
 class SJF :public Processor
 {
 private:
-	PiorityQueue<process*>  sjfqueue;
+	PriorityQueue<process*>  sjfqueue;
 
 public:
-	void addtoready(process* pr)
-	{
-		timeleft = timeleft + pr->getCT();
-		sjfqueue.enqueue(pr, pr->getCT() - pr->getWON());
-		pr->setstate('RDY');
-	}
-
-	process* Schedulealgo()
-	{
-		process* p = nullptr;
-		bool z = sjfqueue.dequeue(p);
-		
-		if (z)
-		{
-			timeleft = timeleft - p->getCT();
-			return p;
-		}
-		else
-			return nullptr;
-	}
-	process* getrun()
-	{
-		return running;
-	}
-	void printdata()
-	{
-		std::cout << sjfqueue.getCount() << "RDY:   ";
-		sjfqueue.printpq();
-	}
-
+	SJF(Schudeler* sch);
+	void addtoready(process* pr);
+	void Schedulealgo(int T);
+	process* getrun();
+	void printdata();
 };
