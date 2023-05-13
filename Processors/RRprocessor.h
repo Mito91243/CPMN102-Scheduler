674b661afe10137c3f3f667d07e2e24c21a93c11
AhmedAshry7 <121507692+AhmedAshry7@users.noreply.github.com>
2023-05-13 08:52:16 +0300

    Update RRprocessor.h

--- Processors/RRprocessor.h
+++ Processors/RRprocessor.h
@@ -9,85 +9,14 @@ private:
 	int timeslice;
 	Queue<process*> Q1;
 	int TOP;
-
+	int RTF;
 public:
 
-	RRprocessor(int ts)
-	{
-		timeslice = ts;
-	}
-	virtual void addtoready(process* pr)
-	{
-		timeleft = timeleft + pr->getCT();
-		Q1.enqueue(pr);
-		pr->setstate('RDY');
-	}
-	virtual process* Schedulealgo()
-	{
-		process* pr = nullptr;
-		bool z = Q1.dequeue(pr);
-
-		if (pr)
-		{
-			timeleft = timeleft - pr->getCT();
-			return pr;
-		}
-		else
-			return nullptr;
-	}
-	process* reachedts(int T)
-	{
-		if (running)
-		{
-			TOP++;
-		}
-		
-		if (TOP == timeslice) 
-		{
-			process* finished = running;
-			process* pr;
-			bool z = Q1.dequeue(pr);
-			
-			if (!z) 
-			{
-				state = 'I';
-				running = nullptr;
-				return finished;
-			}
-			else 
-			{
-				state = 'B';
-				running = pr;
-				running->setstate('RUN');
-
-				timeleft = timeleft - pr->getCT();
-
-				if (running->getRT() == 0) 
-				{
-					running->setRT(T);
-				}
-				TOP = 0;
-				return finished;
-			}
-
-		}
-		else return nullptr;
-	}
-
-	Queue<process*> getqueue()
-	{
-		return Q1;
-	}
-	void printdata()
-	{
-		std::cout << Q1.getCount() << " RDY: ";
-		Q1.printq();
-	}
-	process* getrun()
-	{
-		return running;
-	}
-
-
-
+	RRprocessor(int ts, int rt, Schudeler* sch);
+	virtual void addtoready(process* pr);
+	virtual void Schedulealgo(int T);
+	process* reachedts(int T);
+	Queue<process*> getqueue();
+	void printdata();
+	process* getrun();
 };
